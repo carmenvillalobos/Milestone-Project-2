15509d54284992c6a4db05d16a904b89c51346de
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2023-01-19 19:44:01 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Homepage from './Components/Homepage.jsx';
+import Task from './Components/Task.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+      <Task/>
+    </div>
+  );
+}
+
+export default App;
